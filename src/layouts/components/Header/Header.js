173cc931a1e25ce9915eb8aafb6be09efda1475a
luongmanh1603 <114908089+luongmanh1603@users.noreply.github.com>
2024-03-22 11:28:35 +0700

    home user
    template user

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,28 +1,7 @@
-import {
-    faCoins,
-    faEarthAsia,
-    faEllipsisVertical,
-    faGear,
-    faKeyboard,
-    faQuestionCircle,
-    faSignIn,
-    faSignOut,
-    faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Tippy from '@tippyjs/react';
+
 import classNames from 'classnames/bind';
 import 'tippy.js/dist/tippy.css';
-import { Link } from 'react-router-dom';
-
-import images from '~/assets/images';
-import Button from '~/components/Button';
-import { MessageIcon, NotificationIcon, UploadIcon } from '~/components/Icons';
-import Image from '~/components/Image';
-import Menu from '~/components/Popper/Menu';
 import styles from './Header.module.scss';
-import Search from '~/layouts/components/Search';
-import config from '~/config';
 
 const cx = classNames.bind(styles);
 
